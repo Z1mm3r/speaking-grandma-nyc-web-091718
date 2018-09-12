7ad9ce40f54ac66c8bff2b8f88af976b575d3c51
Casey Zimmer <czimmer@ramapo.edu>
2018-09-12 20:59:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 
 def speak_to_grandma(argument)
-  if (argument === argument.upcase)
-    return "NO, NOT SINCE 1938!."
-  elsif (argument == "I LOVE YOU GRANDMA!")
+  if (argument == "I LOVE YOU GRANDMA!" )
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif (argument == argument.upcase)
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
